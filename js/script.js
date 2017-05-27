77ca50ad227044600cc7191122484a1ae7174c6e
sokal21 <tomas.lautaro.lopez@gmail.com>
2017-05-27 11:37:17 -0300

    Uploading topic 1

---
+++ js/script.js
@@ -0,0 +1,51 @@
+function fadeIn(){
+
+  let hiddenSection = document.querySelector("header.hiddenSection");
+  let opacity = 0;
+  let interval = setInterval(opacityChanger,10);
+
+  function opacityChanger(){
+    if(hiddenSection.style.opacity <= 1){
+      opacity = opacity + 0.005;
+      hiddenSection.style.opacity = opacity;
+    } else {
+      clearInterval(interval);
+    }
+  }
+}
+
+function getJoke(){
+  let xhttp = new XMLHttpRequest();
+
+  xhttp.onreadystatechange = function(){
+    if(xhttp.readyState === xhttp.DONE && this.status == 200){
+      let joke = JSON.parse(xhttp.responseText);
+      let jokeParagraph = document.querySelector(".jokeParagraph");
+
+      console.log(joke);
+      jokeParagraph.innerHTML = joke.value.joke;
+    }
+  };
+
+  xhttp.open("GET","http://api.icndb.com/jokes/random",true);
+  xhttp.send();
+}
+
+function getRepositories(){
+  let list = document.querySelector(".list");
+  let xhttp = new XMLHttpRequest();
+
+  xhttp.onreadystatechange = function(){
+    if(xhttp.readyState === xhttp.DONE && this.status == 200){
+      let repositories = JSON.parse(xhttp.responseText);
+      for (i=0; i<repositories.items.length; i++){
+        let li = document.createElement("li");
+        li.appendChild(document.createTextNode((repositories.items[i]).full_name));
+        list.appendChild(li);
+      }
+    }
+  };
+
+  xhttp.open("GET","https://api.github.com/search/repositories?q=%27JavaScript%27",true);
+  xhttp.send();
+}
